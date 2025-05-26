a2d2db453fe13aaac9fdcb5bde5f08bf164cbea8
Violeta <vakarvioletka@gmail.com>
2025-05-26 13:29:42 +0300

    ерко

--- zav2.c
+++ zav2.c
@@ -2,59 +2,67 @@
 
 int main() {
     int choice;
-    long long product = 1;
-    int count = 0;
 
-    printf("Варіант 15: Добуток всіх чисел, кратних 3, у діапазоні [10; 60]\n");
-    printf("Оберіть тип циклу:\n");
-    printf("1. Цикл for (з параметром)\n");
-    printf("2. Цикл while (з передумовою)\n");
-    printf("3. Цикл do-while (з післяумовою)\n");
-    printf("Ваш вибір: ");
-    scanf("%d", &choice);
+    while (1) {
+        long long product = 1;
+        int count = 0;
+        int i = 10;
 
-    int i = 10;
+        printf("\nВаріант 15: Добуток всіх чисел, кратних 3, у діапазоні [10; 60]\n");
+        printf("Оберіть тип циклу:\n");
+        printf("1. Цикл for (з параметром)\n");
+        printf("2. Цикл while (з передумовою)\n");
+        printf("3. Цикл do-while (з післяумовою)\n");
+        printf("0. Вихід\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    switch (choice) {
-        case 1: // Цикл з параметром
-            for (i = 10; i <= 60; i++) {
-                if (i % 3 == 0) {
-                    product *= i;
-                    count++;
-                }
-            }
+        if (choice == 0) {
+            printf("Завершення програми.\n");
             break;
+        }
 
-        case 2: // Цикл з передумовою
-            while (i <= 60) {
-                if (i % 3 == 0) {
-                    product *= i;
-                    count++;
+        switch (choice) {
+            case 1:
+                for (i = 10; i <= 60; i++) {
+                    if (i % 3 == 0) {
+                        product *= i;
+                        count++;
+                    }
                 }
-                i++;
-            }
-            break;
+                break;
 
-        case 3: // Цикл з післяумовою
-            do {
-                if (i % 3 == 0) {
-                    product *= i;
-                    count++;
+            case 2:
+                while (i <= 60) {
+                    if (i % 3 == 0) {
+                        product *= i;
+                        count++;
+                    }
+                    i++;
                 }
-                i++;
-            } while (i <= 60);
-            break;
+                break;
 
-        default:
-            printf("Некоректний вибір. Спробуйте ще раз.\n");
-            return 1;
-    }
+            case 3:
+                do {
+                    if (i % 3 == 0) {
+                        product *= i;
+                        count++;
+                    }
+                    i++;
+                } while (i <= 60);
+                break;
 
-    if (count > 0) {
-        printf("Кількість чисел, кратних 3: %d\n", count);
-        printf("Добуток цих чисел: %lld\n", product);
-    } else {
-        printf("У діапазоні [10; 60] немає чисел, кратних 3.\n");
+            default:
+                printf("Некоректний вибір. Спробуйте ще раз.\n");
+                continue;
+        }
+
+        if (count > 0) {
+            printf("Кількість чисел, кратних 3: %d\n", count);
+            printf("Добуток цих чисел: %lld\n", product);
+        } else {
+            printf("У діапазоні [10; 60] немає чисел, кратних 3.\n");
+        }
     }
 
     return 0;
