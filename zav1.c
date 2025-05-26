6a19e79f65919f19e45c50722d418f8cc603ca74
Violeta <vakarvioletka@gmail.com>
2025-05-26 21:10:11 +0300

    мрдлол

--- zav1.c
+++ zav1.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
-#include <math.h>    // для sqrt
-#include <stdbool.h> // для bool
+#include <math.h>
+#include <stdbool.h>
 
 int main() {
     int x, y, z;
     double a;
-    bool validInput = false;
+    bool validInput;
+    int repeat;
 
     printf("Завдання 15: Обчислення a = sqrt(x^2 * y - 14 * z)\n");
 
-    // Цикл перевірки правильності вхідних значень
     do {
-        printf("Введіть цілі значення x, y, z через пробіл: ");
-        scanf("%d %d %d", &x, &y, &z);
+        validInput = false;
 
-        // Обчислення підкореневого виразу
-        int sqrt_arg = x * x * y - 14 * z;
+        // Цикл перевірки правильності вхідних значень
+        do {
+            printf("Введіть цілі значення x, y, z через пробіл: ");
+            scanf("%d %d %d", &x, &y, &z);
 
-        // Перевірка на коректність підкореневого виразу
-        if (sqrt_arg < 0) {
-            printf("Помилка: Вираз під коренем (x^2*y - 14*z = %d) < 0. Задайте інші значення.\n", sqrt_arg);
-            validInput = false;
-        } else {
-            a = sqrt(sqrt_arg); // Обчислення значення функції
-            validInput = true;
-        }
+            int sqrt_arg = x * x * y - 14 * z;
 
-    } while (!validInput);
+            if (sqrt_arg < 0) {
+                printf("Помилка: Вираз під коренем (x^2*y - 14*z = %d) < 0. Задайте інші значення.\n", sqrt_arg);
+                validInput = false;
+            } else {
+                a = sqrt(sqrt_arg);
+                validInput = true;
+            }
+        } while (!validInput);
 
-    printf("Вхідні дані коректні.\n");
-    printf("Результат a = %.6lf\n", a);
+        printf("Вхідні дані коректні.\n");
+        printf("Результат a = %.6lf\n", a);
 
+        // Запит на повторне виконання
+        printf("Бажаєте виконати програму ще раз? (1 – так, 0 – вихід): ");
+        scanf("%d", &repeat);
+
+    } while (repeat == 1);
+
+    printf("Програму завершено.\n");
     return 0;
 }
